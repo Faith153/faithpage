5c40138e22361b70347d5f35d34462fd050cd144
Faith153 <157795726+Faith153@users.noreply.github.com>
2025-06-26 18:51:17 +0900

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -6,7 +6,7 @@ your_app/
 └── pages/                ← 추가 페이지들은 이 폴더에 넣기
     ├── 1_imagen.py        ← 페이지 파일명 앞에 숫자 붙이면 순서 지정 가능
     ├── 2_BibleReading.py
-    └── 3_Help.py
+    └── 3_FaithGPT.py
 '''
 
 import streamlit as st
@@ -14,11 +14,11 @@ import streamlit.components.v1 as components
 import pandas as pd
 
 
-st.title("Faith의 코딩 연습장")
-st.write("1-이미지생성기")
-st.write("2-성경읽기표 : 그룹생성, 관리자, 개인별로그인 기능")
+st.title("믿음님의 코딩 연습장")
+st.write("1-이미지생성기 : Imagen")
+st.write("2-성경읽기표 : Bible Reading Checker 그룹생성, 관리자, 개인로그인 기능")
 st.write("  ㄴ예정 : 카카오톡봇을 통한 채팅방에서 명령어로 웹 현황판에 자동 기록?")
-st.write("3-예정 : 우리아이들 맞춤형 ChatGPT")
+st.write("3-FaithGPT-4.1 : 우리아이들 맞춤형 ChatGPT")
 
 # 메인페이지 이미지 첨부(외부링크)
 img_url = "https://images.unsplash.com/photo-1615454782617-e69bbd4f2969?q=80&w=1656&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
