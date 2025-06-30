11f5f166c42ca507fdd8b4b971061d6ccf15e771
Faith153 <157795726+Faith153@users.noreply.github.com>
2025-06-30 16:50:44 +0900

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -19,6 +19,9 @@ st.write("1-이미지생성기 : Imagen")
 st.write("2-성경읽기표 : Bible Reading Checker 그룹생성, 관리자, 개인로그인 기능")
 st.write("  ㄴ예정 : 카카오톡봇을 통한 채팅방에서 명령어로 웹 현황판에 자동 기록?")
 st.write("3-FaithGPT-4.1 : 우리아이들 맞춤형 ChatGPT")
+st.write("4-DD 워크매니저(결제툴)")
+st.write("5-오목 prototype")
+st.write("6-핀볼 prototype_html")
 
 # 메인페이지 이미지 첨부(외부링크)
 img_url = "https://images.unsplash.com/photo-1615454782617-e69bbd4f2969?q=80&w=1656&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
